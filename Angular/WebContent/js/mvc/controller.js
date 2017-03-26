1f44d34b6354b6181ae14a82395cac3ae593c3b3
Rohan Bhalerao <bhalerao.rohan28@gmail.com>
2017-03-26 10:32:39 +0530

    Basic Angular Application

---
+++ Angular/WebContent/js/mvc/controller.js
@@ -0,0 +1,25 @@
+/*
+
+function myFirstController($scope) {  
+
+	$scope.title = "hello rohan";   
+}
+
+function mainController($scope) {
+	$scope.message = 'Everyone come and see how good I look!';
+}
+
+function aboutController($scope) {
+	$scope.message = 'Look! I am an about page.';
+}
+
+function contactController($scope) {
+	$scope.message = 'Contact us! JK. This is just a demo.';
+}
+
+angular.module('AngularMania',[])
+		.controller("myCtrl",myFirstController )
+		.controller("mainController",mainController )
+		.controller("aboutController",aboutController )
+		.controller("contactController",contactController ); 
+*/
